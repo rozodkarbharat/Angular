fed202c3d00e76f092d2fc065af237025c4dce92
rozodkarbharat <99673832+rozodkarbharat@users.noreply.github.com>
2023-06-17 22:01:21 +0530

    ViewChild

--- Angular/hotelinventory/src/app/rooms/rooms-list/rooms-list.component.ts
+++ Angular/hotelinventory/src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,17 +1,27 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output, SimpleChange, SimpleChanges } from '@angular/core';
 import { RoomList } from '../rooms';
 
 @Component({
   selector: 'hinv-rooms-list',
   templateUrl: './rooms-list.component.html',
-  styleUrls: ['./rooms-list.component.css']
+  styleUrls: ['./rooms-list.component.css'],
+  // changeDetection:ChangeDetectionStrategy.OnPush
 })
-export class RoomsListComponent {
+export class RoomsListComponent implements OnChanges {
 
+  @Input() title : string=''
   @Input() rooms:RoomList[]=[]
   @Output() selectedRoom= new EventEmitter <RoomList>()
 
   selectRoom(room:RoomList){
     this.selectedRoom.emit(room)
   }
+  ngOnChanges(changes: SimpleChanges) {
+    // changes.prop contains the old and the new value...
+    console.log(changes)
+    if(changes['title']){
+
+      this.title=changes['title'].currentValue.toUpperCase()
+    }
+}
 }
--- Angular/hotelinventory/src/app/rooms/rooms.component.ts
+++ Angular/hotelinventory/src/app/rooms/rooms.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnChanges, SimpleChange, ViewChild } from '@angular/core';
 import { Room, RoomList } from './rooms';
+import { RoomsListComponent } from './rooms-list/rooms-list.component';
+import { Element } from '@angular/compiler';
 
 @Component({
   selector: 'hinv-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.css'],
 })
-export class RoomsComponent {
+export class RoomsComponent implements AfterViewInit{
   hotelName = 'Taj hotel';
   numberOfRooms = 10;
 
-  selecteRoom!:RoomList
+// @ViewChild("rooms-list")child:RoomsListComponent
+@ViewChild("roomBtn")btn!:ElementRef<any>
 
+  selecteRoom!:RoomList
+  title="Room List"
   hideRooms = false;
   rooms: Room = {
     totalRooms: 15,
@@ -56,11 +61,35 @@ export class RoomsComponent {
   ];
 
   toggle() {
+    console.log(this.title)
     this.hideRooms = !this.hideRooms;
+    this.title= "Rooms List"
   }
 
   selectedRoom(room:RoomList){
     console.log(room)
     this.selecteRoom=room
   }
+  addRoom(){
+    const room:RoomList={
+
+      roomNumber:4,
+      roomtype: 'Private suite',
+      amenities: 'Air conditioner, free wifi water',
+      photos:
+        'https://media.istockphoto.com/id/1369575590/photo/living-room-inderior-with-arch-windows-furnished-with-modern-sofa-and-dining-table-3d-render.jpg?s=612x612&w=is&k=20&c=EAPH4xFM6DtFyIvFXM1ALdzG2WlEsJ_Xcrh68AB3an8=',
+      price: 1500,
+      checkInDate: new Date('11-Nov-2021'),
+      checkOutDate: new Date('11-Nov-2021'),
+      rating:3.5
+    }
+    this.roomList.push(room)
+
+  }
+
+  ngAfterViewInit(){
+    console.log(this.btn.nativeElement.style.backgroundColor="green")
+  }
+
+
 }
