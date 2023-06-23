143dc06dee10656db9c2d67700df5718c319b15a
rozodkarbharat <99673832+rozodkarbharat@users.noreply.github.com>
2023-06-23 21:44:10 +0530

    routing

---
+++ Angular/hotelinventory/src/app/rooms/rooms-booking/rooms-booking.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hinv-rooms-booking',
+  templateUrl: './rooms-booking.component.html',
+  styleUrls: ['./rooms-booking.component.css']
+})
+export class RoomsBookingComponent {
+
+}
